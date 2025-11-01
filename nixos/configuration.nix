b93063695f116202a6842c92fc0710dc9f65816d
Mark Wang <markwangfreetyle@gmail.com>
2025-11-02 07:34:17 +1100

    add .local/bin to path

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -186,9 +186,14 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Ensure GUI applications have access to user binaries
+  environment.sessionVariables = {
+    PATH = [ "$HOME/.local/bin" ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
